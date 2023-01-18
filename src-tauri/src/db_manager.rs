d5c9a7cdf7d4c2000e2870f250ea6c273f4fffd4
DevArthurCruz <developer.arthur.cruz@gmail.com>
2023-01-18 00:08:48 -0300

    FEAT[DBManager]: Implemented the beginning of the migrate method in DBManager. Currently trying to read from the migrations directory and ordering the migration files alphabetically.
    develop

--- src-tauri/src/db_manager.rs
+++ src-tauri/src/db_manager.rs
@@ -1,7 +1,10 @@
 const DB_NAME: &str = "rusty_time_note.db";
+const DB_MIGRATIONS_PATH: &str = "./migrations";
 
-use log::error;
+use log::{error, info};
 use rusqlite::Connection;
+use std::fs::{self, DirEntry, ReadDir};
+use std::io;
 use std::{
     process::exit,
     sync::{Arc, Mutex},
@@ -23,5 +26,48 @@ impl DBManager {
         DBManager {
             connection: Arc::new(Mutex::new(conn)),
         }
+    }
+
+    pub fn migrate(&self) -> Result<(), String> {
+        let mut files: Vec<DirEntry> = match fs::read_dir(DB_MIGRATIONS_PATH) {
+            Ok(paths) => paths
+                .map(|file| file.unwrap())
+                .filter(|file| {
+                    file.path().is_file()
+                        && file
+                            .file_name()
+                            .to_ascii_lowercase()
+                            .to_str()
+                            .unwrap()
+                            .ends_with("sql")
+                })
+                .collect::<Vec<DirEntry>>(),
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::NotFound => Vec::new(),
+                _ => {
+                    error!("Failed to read db migrations: {}", e);
+                    return Err(e.to_string());
+                }
+            },
+        };
+
+        info!("{} files found in migrations directory", { files.len() });
+
+        files.sort_by(|a, b| {
+            a.file_name()
+                .to_ascii_lowercase()
+                .cmp(&b.file_name().to_ascii_lowercase())
+        });
+
+        for file in files.iter() {
+            info!(
+                "File name: {}",
+                file.file_name()
+                    .to_str()
+                    .unwrap_or("error getting file name")
+            )
+        }
+
+        Ok(())
     }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,8 @@ mod db_manager;
 
 use db_manager::DBManager;
 use fast_log::Config;
-
+use log::{error, Log};
+use std::process::exit;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -16,7 +17,7 @@ fn greet(name: &str) -> String {
 
 fn main() {
     let _ = std::fs::remove_file("./rusty_time_note.log");
-    fast_log::init(
+    let logger = fast_log::init(
         Config::new()
             .file("rusty_time_note.log")
             .console()
@@ -24,8 +25,16 @@ fn main() {
     )
     .unwrap();
 
+    let db_manager = DBManager::new();
+
+    if let Err(error_string) = db_manager.migrate() {
+        error!("Failed to migrate local database: {}", error_string);
+        logger.flush();
+        exit(-1);
+    }
+
     tauri::Builder::default()
-        .manage(DBManager::new())
+        .manage(db_manager)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
